a006617c2c92caa228b7affb56e149b4b23ad658
ahasanhossen <ahasanhossen57@gmail.com>
2021-11-25 01:55:31 +0600

    UserAccout migration created

---
+++ database/migrations/2021_11_24_194923_create_user_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('username')->unique();
+            $table->string('role');
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_accounts');
+    }
+}
