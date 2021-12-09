b174e114d5fe9d5d453c91b73730dd8fbf1ae88a
avishekchy45 <avishekchy45@gmail.com>
2021-12-10 00:48:18 +0600

    Teacher Pages Updated

--- resources/views/includes/teasidebar.blade.php
+++ resources/views/includes/teasidebar.blade.php
@@ -1,6 +1,6 @@
 <ul class='nav flex-column nav-pills nav-fill' style="margin-top: 0.5rem;">
     <li class='nav-item'>
-        <span class="power" ><strong>E X C L U S I V E &nbsp; P O W E R S</strong> </span>
+        <span class="power"><strong>E X C L U S I V E &nbsp; P O W E R S</strong> </span>
     </li>
     <li class='nav-item'>
         <a href="teacher" style="text-decoration: none">
@@ -10,6 +10,31 @@
     <li class='nav-item'>
         <a href="enrollstudent" style="text-decoration: none">
             <div class="sidebaropt">Enrollment</div>
+        </a>
+    </li>
+    <li class='nav-item'>
+        <a href="evaluatecourse" style="text-decoration: none">
+            <div class="sidebaropt">Evaluation</div>
+        </a>
+    </li>
+    <li class='nav-item'>
+        <a href="classroutine" style="text-decoration: none">
+            <div class="sidebaropt">Class Routine</div>
+        </a>
+    </li>
+    <li class='nav-item'>
+        <a href="examoutine" style="text-decoration: none">
+            <div class="sidebaropt">Exam Routine</div>
+        </a>
+    </li>
+    <li class='nav-item'>
+        <a href="results" style="text-decoration: none">
+            <div class="sidebaropt">Detailed Results</div>
+        </a>
+    </li>
+    <li class='nav-item'>
+        <a href="reports" style="text-decoration: none">
+            <div class="sidebaropt">Reports</div>
         </a>
     </li><br>
 </ul>
